41184ed7080b894151c229182dc73b568091632b
KuroChimera <kurocanidae@gmail.com>
2023-07-19 12:10:48 -0400

    Added playerChoice prompt and options

--- rps-scripts.js
+++ rps-scripts.js
@@ -1,3 +1,16 @@
+const playerChoice = () => {
+  const playerChoice = prompt("Enter you weapon choice (Rock, Paper, or Scissors)", "").toLowerCase();
+  if (playerChoice = "rock") {
+    return playerChoice
+  }
+  if (playerChoice = "paper") {
+    return playerChoice
+  }
+  if (playerChoice = "scissors") {
+    return playerChoice
+  }
+}
+
 const getComputerChoice = () => {
   const randomChoice =  Math.floor(Math.random() * 3);
   if (randomChoice === 0) {
